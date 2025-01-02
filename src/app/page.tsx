4bdddc1ec8ce5a5022697e7c7dedb0ea18bd677c
Dean Wooding <dbwooding88@gmail.com>
2025-01-02 10:50:43 +0800

    'puglic'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-gray-800">Hello, World!</h1>
-    </main>
-  )
+  return <div>Hello World</div>
 }
